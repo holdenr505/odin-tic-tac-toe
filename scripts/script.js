313941f31c0fda233c987e561916d59dd78c4bf4
omni23 <89713821+omni23@users.noreply.github.com>
2023-04-19 17:08:58 -0400

    Add game functions to gameController

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// PLAYER
+
 const Player = (name, sign) => {
   const getName = () => name;
   const getSign = () => sign;
@@ -5,21 +7,67 @@ const Player = (name, sign) => {
   return { getName, getSign };
 };
 
+// GAMEBOARD
+
 const gameBoard = (function () {
-  const _boardTiles = new Array(9);
+  const _boardTiles = Array(9).fill("");
 
   const setBoardTile = (sign, index) => {
     _boardTiles[index] = sign;
   };
 
-  const getBoardTile = (index) => _boardTiles[index];
+  const getBoard = () => _boardTiles;
 
-  return { setBoardTile, getBoardTile };
+  return { setBoardTile, getBoard };
 })();
+
+// GAME CONTROLLER
 
 const gameController = (function () {
-  return {};
+  let _playerOne = null;
+  let _playerTwo = null;
+  let _currentPlayer = _playerOne;
+  let _roundCount = 1;
+
+  const initPlayers = (nameOne, signOne, nameTwo, signTwo) => {
+    _playerOne = Player(nameOne, signOne);
+    _playerTwo = Player(nameTwo, signTwo);
+  };
+
+  const _checkWin = (board) => {
+    const winStates = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    const winner = winStates.filter(
+      (state) =>
+        state.every((index) => board[index] === "x") ||
+        state.every((index) => board[index] === "o")
+    )[0];
+
+    return winner;
+  };
+
+  // playRound can call checkWin (helper function)
+  const playRound = (board, index) => {
+    board.setBoardTile(_currentPlayer.getSign(), index);
+
+    // alternate between players and update round count
+    _currentPlayer = _playerOne ? _playerTwo : _playerOne;
+    _roundCount += 1;
+  };
+
+  return { initPlayers, playRound };
 })();
+
+// DISPLAY
 
 const displayController = (function () {
   // functions to handle event listeners
@@ -28,24 +76,31 @@ const displayController = (function () {
   const _interfaceTiles = document.querySelectorAll(".tile");
   const _playerInfo = document.querySelectorAll("input");
 
-  //we'll have to handle this based on player turn
-  _startButton.addEventListener("click", () => {
-    gameController.initPlayers(
-      _playerInfo[0],
-      _playerInfo[1],
-      _playerInfo[2],
-      _playerInfo[3]
-    );
-    _initListeners();
-  });
-
   const _initListeners = () => {
     _interfaceTiles.forEach((tile) =>
       tile.addEventListener("click", () => {
-        // do something
+        // ideally there's a way to check only after at least
+        // 3 rounds
+        // call playRound and also update DOM
       })
     );
   };
+
+  // we'll have to handle this based on player turn
+  _startButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    gameBoard.resetBoard();
+
+    gameController.initPlayers(
+      _playerInfo[0].value,
+      _playerInfo[1].value,
+      _playerInfo[2].value,
+      _playerInfo[3].value
+    );
+    _initListeners();
+  });
 
   return {};
 })();
